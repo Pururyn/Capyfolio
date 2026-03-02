7a33c388a206a0e4988e5b457ff37c342da87254
Lynn <jacquelinehuynh@outlook.fr>
2026-03-02 11:59:45 +0100

    Repoping header

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.addEventListener('scroll', () => {
     const backToTopButton = document.querySelector('.back-to-top');
+    const nav = document.querySelector('nav'); // On récupère ton header
 
-    // On affiche le bouton si on a scrollé plus de 300 pixels
+    // On affiche les deux si on a scrollé plus de 300 pixels
     if (window.scrollY > 300) {
         backToTopButton.classList.add('visible');
+        nav.classList.add('visible');
     } else {
         backToTopButton.classList.remove('visible');
+        nav.classList.remove('visible');
     }
 });
